981ca43c8abb449583dc8fa4a0cd5208369dab36
Jannis Pohlmann <jannis@xfce.org>
2015-12-07 20:57:56 +0100

    Start implementing component and requirement components

--- src/web/components/components.cljs
+++ src/web/components/components.cljs
@@ -2,26 +2,79 @@
   (:require [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+(defui Parent
+  static om/Ident
+  (ident [this props]
+    [:node (:name props)])
+  static om/IQuery
+  (query [this]
+    [:name :title])
+  Object
+  (render [this]
+    (dom/div nil
+      (:title (om/props this)))))
+
+(def parent* (om/factory Parent))
+
+(declare component)
+
 (defui Component
   static om/Ident
   (ident [this props]
-    [:component (:id props)])
+    [:node (:name props)])
   static om/IQuery
   (query [this]
-    [:id :title :description])
+    [:name :title :description
+     {:parent (om/get-query Parent)}
+     {:children '...}])
   Object
-  (render [this]
-    (let [{:keys [id title description]} (om/props this)]
-      description)))
+  (toggle-expanded [this]
+    (om/update-state! this update :expanded not))
 
-(def component (om/factory Component {:key-fn :id}))
+  (render [this]
+    (let [{:keys [name title description children]} (om/props this)
+          {:keys [parent]} (om/get-computed this)
+          {:keys [expanded]} (om/get-state this)]
+      (dom/div #js {:className
+                    (str "node"
+                         (when-not parent
+                           " node-root"))}
+        (dom/h2 #js {:className "node-header"
+                     :onClick #(.toggle-expanded this)}
+          (dom/span #js {:className "node-header-title"} title)
+          (dom/span #js {:className "node-header-name"} name))
+        (dom/div #js {:className
+                      (str "node-details"
+                           (if expanded
+                             " node-details-expanded"
+                             " node-details-collapsed"))}
+          (dom/div #js {:className "node-details-table"}
+            (when description
+              (dom/div #js {:className "node-detail"}
+                (dom/div #js {:className "node-detail-label"}
+                  "Description")
+                (dom/div #js {:className "node-detail-content"}
+                  description)))
+            (when parent
+              (dom/div #js {:className "node-detail"}
+                (dom/div #js {:className "node-detail-label"}
+                  "Parent")
+                (dom/div #js {:className "node-detail-content"}
+                  (parent* parent))))))
+        (dom/div #js {:className "node-subnodes"}
+          (for [child children]
+            (component
+              (om/computed child {:parent (om/props this)}))))))))
+
+(def component (om/factory Component {:key-fn :name}))
 
 (defui Components
   Object
   (render [this]
+    (println (om/props this))
     (let [items (:components (om/props this))]
-      (dom/div #js {:className "components"}
-        (for [item items]
+      (dom/div #js {:className "nodes"}
+        (for [item (filter #(nil? (:parent %)) items)]
           (component item))))))
 
-(def components (om/factory Components {:keyfn :id}))
+(def components (om/factory Components))
--- src/web/components/requirements.cljs
+++ src/web/components/requirements.cljs
@@ -2,23 +2,69 @@
   (:require [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+(defui Parent
+  static om/Ident
+  (ident [this props]
+    [:node (:name props)])
+  static om/IQuery
+  (query [this]
+    [:name :title])
+  Object
+  (render [this]
+    (dom/div nil
+      (:title (om/props this)))))
+
+(def parent* (om/factory Parent))
+
 (declare requirement)
 
 (defui Requirement
   static om/Ident
   (ident [this props]
-    [:requirement (:name props)])
+    [:node (:name props)])
   static om/IQuery
   (query [this]
-    '[:name :title {:children '...}])
+    [:name :title :description
+     {:parent (om/get-query Parent)}
+     {:children '...}])
   Object
+  (toggle-expanded [this]
+    (om/update-state! this update :expanded not))
+
   (render [this]
-    (let [{:keys [name title children]} (om/props this)]
-      (dom/div #js {:className "requirement"}
-        (dom/h2 #js {:className "requirement-header"} title)
-        (dom/div #js {:className "requirement-subrequirements"}
+    (let [{:keys [name title description children]} (om/props this)
+          {:keys [parent]} (om/get-computed this)
+          {:keys [expanded]} (om/get-state this)]
+      (dom/div #js {:className
+                    (str "node"
+                         (when-not parent
+                           " node-root"))}
+        (dom/h2 #js {:className "node-header"
+                     :onClick #(.toggle-expanded this)}
+          (dom/span #js {:className "node-header-title"} title)
+          (dom/span #js {:className "node-header-name"} name))
+        (dom/div #js {:className
+                      (str "node-details"
+                           (if expanded
+                             " node-details-expanded"
+                             " node-details-collapsed"))}
+          (dom/div #js {:className "node-details-table"}
+            (when description
+              (dom/div #js {:className "node-detail"}
+                (dom/div #js {:className "node-detail-label"}
+                  "Description")
+                (dom/div #js {:className "node-detail-content"}
+                  description)))
+            (when parent
+              (dom/div #js {:className "node-detail"}
+                (dom/div #js {:className "node-detail-label"}
+                  "Parent")
+                (dom/div #js {:className "node-detail-content"}
+                  (parent* parent))))))
+        (dom/div #js {:className "node-subnodes"}
           (for [child children]
-            (requirement child)))))))
+            (requirement
+              (om/computed child {:parent (om/props this)}))))))))
 
 (def requirement (om/factory Requirement {:key-fn :name}))
 
@@ -27,8 +73,8 @@
   (render [this]
     (println (om/props this))
     (let [items (:requirements (om/props this))]
-      (dom/div #js {:className "requirements"}
-        (for [item items]
+      (dom/div #js {:className "nodes"}
+        (for [item (filter #(nil? (:parent %)) items)]
           (requirement item))))))
 
 (def requirements (om/factory Requirements))
