52366b7687002c99e4ae45d97ac217a09ef50451
Jannis Pohlmann <jannis@xfce.org>
2015-12-10 17:35:35 +0100

    Only render project description and copyright if they are set

--- src/web/components/project.cljs
+++ src/web/components/project.cljs
@@ -9,19 +9,21 @@
     [:node (:name props)])
   static om/IQuery
   (query [this]
-    [:name :title :description :copyright :sort-by])
+    [:name :title :description :copyright])
   Object
   (render [this]
     (let [{:keys [name title description
                   copyright sort-by]} (om/props this)]
       (dom/div #js {:className "node node-root"}
         (dom/h2 #js {:className "node-header"} title)
-        (dom/div #js {:className "project-description"}
-          (markdown {:text description}))
-        (dom/div #js {:className "project-copyright"}
-          (dom/h3 #js {:className "project-copyright-header"}
-            "Copyright")
-          (dom/div #js {:className "project-copyright-text"}
-            (markdown {:text copyright})))))))
+        (if description
+          (dom/div #js {:className "project-description"}
+            (markdown {:text description})))
+        (if copyright
+          (dom/div #js {:className "project-copyright"}
+            (dom/h3 #js {:className "project-copyright-header"}
+              "Copyright")
+            (dom/div #js {:className "project-copyright-text"}
+              (markdown {:text copyright}))))))))
 
 (def project (om/factory Project {:key-fn :name}))
